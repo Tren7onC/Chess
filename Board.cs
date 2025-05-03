873d586b633998ed8eafb06c3d0dccfb8625371c
Trenton Cuperus <Trenton.Cuperus@trojans.dsu.edu>
2025-05-03 15:26:56 -0500

    Basic Board Set up with intial sceen for the board

---
+++ Board.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Chess
+{
+    //Need to include The Form as a super class beacuse of the Controls.
+    //The method Find the button thing needs this so I can actually look through 
+    // the buttons and not go through each indivially with a switch 
+    internal class Board : Form
+    {
+
+        public int[,] board = new int[8, 8];
+
+        /*
+        Piece numbers for reference:
+   
+        1 -  8  = PLayer 1 pawns
+        9 - 10  = Player 1 Rook
+        11 - 12 = Player 1 Horses
+        13 - 14 = Player 1 Bishops
+        15      = Player 1 Queen
+        16      = Player 1 King
+
+        17 - 24 = Player 2 pawns
+        25 - 26 = Player 2 Rook
+        27 - 28 = Player 2 Horses
+        29 - 30 = Player 2 Bishops
+        31      = Player 2 Queen
+        32      = Player 1 King
+        */
+        public Board()
+        {
+            //Set up for intial Board
+                //Row 7 for Player 1/ White Team
+                board[7, 0] = 9; board[7, 1] = 11; board[7, 2] = 13; board[7, 3] = 15; board[7, 4] = 16; board[7, 5] = 14; board[7, 6] = 12; board[7, 7] = 10;
+                //Row 6 for Player 1/ White Team
+                board[6, 0] = 1; board[6, 1] = 2; board[6, 2] = 3; board[6, 3] = 4; board[6, 4] = 5; board[6, 5] = 6; board[6, 6] = 7; board[6, 7] = 8;
+
+                //Row 0 for Player 1/ Black Team
+                board[0, 0] = 25; board[0, 1] = 27; board[0, 2] = 29; board[0, 3] = 31; board[0, 4] = 32; board[0, 5] = 26; board[0, 6] = 28; board[0, 7] = 30;
+                //Row 1 for Player 1/ Black Team
+                board[1, 0] = 17; board[1, 1] = 18; board[1, 2] = 19; board[1, 3] = 20; board[1, 4] = 21; board[1, 5] = 22; board[1, 6] = 23; board[1, 7] = 24;
+        
+        }
+
+
+    }
+
+}
--- Form1.cs
+++ Form1.cs
@@ -12,9 +12,131 @@ namespace Chess
 {
     public partial class Form1 : Form
     {
+        Board Chessboard = new Board();
+
         public Form1()
         {
             InitializeComponent();
         }
+
+
+        private void HandleButtonClick(object sender, EventArgs e)
+        {
+            Button clickedButton = sender as Button;
+            TextBox.Text = String.Format($"{clickedButton.Tag}");
+            DrawBoard();
+        }
+
+        public void DrawBoard()
+        {
+            string tag;
+            int piece;
+            Button targetbutton = null;
+            for (int row = 0; row < 8; row++)
+            {
+                for (int col = 0; col < 8; col++)
+                {
+                    tag = $"{row}{col}";
+                    piece = Chessboard.board[row, col];
+
+                    foreach (Control control in this.Controls)
+                    {
+                        if (control is Button btn && btn.Tag != null && btn.Tag.ToString() == tag)
+                        {
+                            targetbutton = btn;
+                            break;
+                        }
+                    }
+                    if (targetbutton != null)
+                    {
+                        if (piece >= 1 && piece <= 8)
+                        {
+                            //White Pawns
+                            targetbutton.BackgroundImage = Properties.Resources.WhitePawn;
+                            targetbutton.ImageAlign = ContentAlignment.MiddleCenter;
+                            targetbutton.BackgroundImageLayout = ImageLayout.Zoom;
+                        }
+                        else if(piece == 9 || piece == 10)
+                        {
+                            //White Rook
+                            targetbutton.BackgroundImage = Properties.Resources.WhiteRook;
+                            targetbutton.ImageAlign = ContentAlignment.MiddleCenter;
+                            targetbutton.BackgroundImageLayout = ImageLayout.Zoom;
+                        }
+                        else if (piece == 11 || piece == 12)
+                        {
+                            //White Knight
+                            targetbutton.BackgroundImage = Properties.Resources.WhiteKnight;
+                            targetbutton.ImageAlign = ContentAlignment.MiddleCenter;
+                            targetbutton.BackgroundImageLayout = ImageLayout.Zoom;
+                        }
+                        else if (piece == 13 || piece == 14)
+                        {
+                            //White Bishop
+                            targetbutton.BackgroundImage = Properties.Resources.WhiteBishop;
+                            targetbutton.ImageAlign = ContentAlignment.MiddleCenter;
+                            targetbutton.BackgroundImageLayout = ImageLayout.Zoom;
+                        }
+                        else if (piece == 15)
+                        {
+                            //White Queen
+                            targetbutton.BackgroundImage = Properties.Resources.WhiteQueen;
+                            targetbutton.ImageAlign = ContentAlignment.MiddleCenter;
+                            targetbutton.BackgroundImageLayout = ImageLayout.Zoom;
+                        }
+                        else if (piece == 16)
+                        {
+                            //White King
+                            targetbutton.BackgroundImage = Properties.Resources.WhiteKing;
+                            targetbutton.ImageAlign = ContentAlignment.MiddleCenter;
+                            targetbutton.BackgroundImageLayout = ImageLayout.Zoom;
+                        }
+                        else if (piece >= 17 && piece <= 24)
+                        {
+                            //Black Pawn
+                            targetbutton.BackgroundImage = Properties.Resources.BlackPawn;
+                            targetbutton.ImageAlign = ContentAlignment.MiddleCenter;
+                            targetbutton.BackgroundImageLayout = ImageLayout.Zoom;
+                        }
+                        else if (piece == 25 || piece == 26)
+                        { 
+                            //Black Rook
+                            targetbutton.BackgroundImage = Properties.Resources.BlackRook;
+                            targetbutton.ImageAlign = ContentAlignment.MiddleCenter;
+                            targetbutton.BackgroundImageLayout = ImageLayout.Zoom;
+                        }
+                        else if (piece == 27 || piece == 28)
+                        {
+                            //Black Rook
+                            targetbutton.BackgroundImage = Properties.Resources.BlackKnight;
+                            targetbutton.ImageAlign = ContentAlignment.MiddleCenter;
+                            targetbutton.BackgroundImageLayout = ImageLayout.Zoom;
+                        }
+                        else if (piece == 29 || piece == 30)
+                        {
+                            //Black Rook
+                            targetbutton.BackgroundImage = Properties.Resources.BlackBishop;
+                            targetbutton.ImageAlign = ContentAlignment.MiddleCenter;
+                            targetbutton.BackgroundImageLayout = ImageLayout.Zoom;
+                        }
+                        else if (piece == 31)
+                        {
+                            //Black Rook
+                            targetbutton.BackgroundImage = Properties.Resources.BlackQueen;
+                            targetbutton.ImageAlign = ContentAlignment.MiddleCenter;
+                            targetbutton.BackgroundImageLayout = ImageLayout.Zoom;
+                        }
+                        else if (piece == 32)
+                        {
+                            //Black Rook
+                            targetbutton.BackgroundImage = Properties.Resources.BlackKing;
+                            targetbutton.ImageAlign = ContentAlignment.MiddleCenter;
+                            targetbutton.BackgroundImageLayout = ImageLayout.Zoom;
+                        }
+                    }
+                }
+            }
+        }
+
     }
 }
